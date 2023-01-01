dcff9c0f4c013daa72f8d9deaa469c4ff3f3f374
kmabhinai <kmabhinai@gmail.com>
2023-01-01 11:18:22 +0530

    Prettified Code!

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const cards=document.querySelectorAll('.card');
-var flipped=false,first_card,second_card,score=0;
+const cards = document.querySelectorAll('.card');
+var flipped = false, first_card, second_card, score = 0;
 
-function flip(){
+function flip() {
     this.classList.add("flip");
-    if(!flipped){
-        flipped=true;
-        first_card=this;
+    if (!flipped) {
+        flipped = true;
+        first_card = this;
     }
-    else{
-        second_card=this;
+    else {
+        second_card = this;
         check();
-        cards.forEach((card)=>{card.removeEventListener("click",flip);})
-        cards.forEach((card)=> card.addEventListener("click",flip));
+        cards.forEach((card) => { card.removeEventListener("click", flip); });
+        cards.forEach((card) => card.addEventListener("click", flip));
     }
 }
 
-function check(){
-    if(first_card.dataset.image==second_card.dataset.image && first_card.dataset.num!=second_card.dataset.num){
-        
-        first_card.removeEventListener("click",flip);
-        second_card.removeEventListener("click",flip);
-        
+function check() {
+    if (first_card.dataset.image == second_card.dataset.image && first_card.dataset.num != second_card.dataset.num) {
+
+        first_card.removeEventListener("click", flip);
+        second_card.removeEventListener("click", flip);
+
         reset();
-        score+=100;
-        document.getElementById("score").innerText="Score : "+score;
-        if(score==800){
-            document.getElementById("success").innerText="You Won the Game!🎉";
+        score += 100;
+        document.getElementById("score").innerText = "Score : " + score;
+        if (score == 800) {
+            document.getElementById("success").innerText = "You Won the Game!🎉";
 
         }
     }
@@ -34,21 +34,21 @@ function check(){
             first_card.classList.remove("flip");
             second_card.classList.remove("flip");
             reset();
-        },350);
+        }, 350);
     }
 }
 
-function reset(){
-    [first_card,second_card,flipped]=[null,null,false];
+function reset() {
+    [first_card, second_card, flipped] = [null, null, false];
 }
 
-(function shuffle(){
-    cards.forEach((card)=>{
-        var index=Math.floor(Math.random()*16);
-        card.style.order=index;
-    })
+(function shuffle() {
+    cards.forEach((card) => {
+        var index = Math.floor(Math.random() * 16);
+        card.style.order = index;
+    });
 })();
 
 
 //Main
-cards.forEach((card)=> card.addEventListener("click",flip));
+cards.forEach((card) => card.addEventListener("click", flip));
